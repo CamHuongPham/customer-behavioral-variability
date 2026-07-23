04050c220691e81a7ff905830c45a0fb046479e7
Cam H. Pham <camhuongpham.p@gmail.com>
2026-07-24 06:08:08 +0700

    Clean and preprocess transaction data

--- 01_data_preprocessing.R
+++ 01_data_preprocessing.R
@@ -1,7 +1,6 @@
 library(readxl)
 library(dplyr)
 
-
 retail <- bind_rows(
   read_excel("D:/online+retail+ii/online_retail_II.xlsx", sheet = "Year 2009-2010"),
   read_excel("D:/online+retail+ii/online_retail_II.xlsx", sheet = "Year 2010-2011")
